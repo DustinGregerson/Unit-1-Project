5404676af8071282c125dc48e2e4bdfa4f47da7e
DustinGregerson <dustin.gregerson@southeasttech.edu>
2023-09-13 21:45:00 -0500

    Created a CubeAreaForm Created CubeControler Created CubeModel Created Index View Created Results View Discovered you can NOT call the _ViewInports.cshtml what ever you want

---
+++ Unit 1 Project/Controllers/CubeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unit_1_Project.Controllers
+{
+    public class Controller : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Unit 1 Project/Models/CubeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Unit_1_Project.Models
+{
+    public class CubeModel
+    {
+        public string? width { get; set; }
+        public string? height { get; set; }
+
+        public string? length { get; set; }
+
+        
+    }
+}
