dc4b396b5698c488f3ad30ee41395af092c567e6
DustinGregerson <dustin.gregerson@southeasttech.edu>
2023-09-14 18:33:43 -0500

    Discovered that the default home controller pattern does not always work And that the the pattern may need to point to the a controller that is created. not only that but also discovered that asp may search through the shared views folder over the veiws folder its self for the controlers index page.

--- Unit 1 Project/Controllers/Cube.cs
+++ Unit 1 Project/Controllers/Cube.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Unit_1_Project.Models;
 namespace Unit_1_Project.Controllers
 {
-    public class Controller : Microsoft.AspNetCore.Mvc.Controller
+    public class Cube : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Display(CubeModel model)
+        {
+            return View(model);
         }
     }
 }
--- Unit 1 Project/Models/CubeModel.cs
+++ Unit 1 Project/Models/CubeModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Unit_1_Project.Models
+﻿using System.Text.RegularExpressions;
+
+namespace Unit_1_Project.Models
 {
     public class CubeModel
     {
@@ -7,6 +9,82 @@
 
         public string? length { get; set; }
 
-        
+        public double areaCube { get; set; }
+
+        //all bits set to zero
+        int bitFlags = 0;
+
+        //message to represent each flag
+        string[] errorAt = 
+        {"width",
+         "height",
+         "length"};
+
+        //final error message
+        string errorMessage = "";
+        public void ValidateStrings()
+        {
+            //regex to check if the value entered contains only numbers
+            string AllNumberPattern = @"^(\d+)$|^(\d+\.\d+)$|^(\.\d+)$";
+            Regex NumberCheck = new Regex(AllNumberPattern);
+
+            //sets the bit flags
+            if (width == null) {
+                bitFlags = 1;
+            }
+            else if (NumberCheck.Match(width).Success)
+            {
+               
+            }
+            else
+            {
+                bitFlags = bitFlags | 0x2;
+            }
+
+            if (height == null)
+            {
+                bitFlags = bitFlags | 0x4;
+            }
+            else if (NumberCheck.Match(height).Success)
+            {
+                
+            }
+            else
+            {
+                bitFlags |= 0x8;
+            }
+
+            if (length==null)
+            {
+                bitFlags = bitFlags | 0x10;
+            }
+            else if (NumberCheck.Match(length).Success)
+            {
+                
+            }
+            else
+            {
+                bitFlags = bitFlags | 0x20;
+            }
+
+        }
+
+        public string ErrorMessageParser()
+        {
+            int mask=0x3;
+            for(int i=0;i<errorAt.Length;i++)
+            {
+                switch((bitFlags >> i * 2)&mask)
+                {
+                    case 0x2: errorMessage += "You have to enter a numeric value in the "+errorAt[i]+" text feild";
+                        break;
+                    case 0x4:
+                              errorMessage += "You Must enter a valid number ie (1.1, 1, .1) in the" + errorAt[i] + " text feild";
+                    break;
+                }
+                
+            }
+            return errorMessage;
+        }
     }
 }
